e2f450e804f3aeceddaab61f9f9665b4741e57e1
Jemal Agayeva <Jemal.agayeva@gmail.com>
2020-12-21 15:20:32 -0500

    PUBG ninjas hide and show project is finalized

---
+++ script.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function () {
+    $('img').click(function(){
+        $(this).hide()
+        });
+    $('button').click(function(){
+        $('#grid img').show()
+    });
+
+
+
+    });
+
+
+
+
+
